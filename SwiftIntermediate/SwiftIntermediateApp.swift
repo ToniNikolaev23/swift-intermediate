bd66ccfe3dfd9cf0c1abb61604fdc20f5503c2c2
Toni Stoyanov <toni.n.stoyanov@icloud.com>
2024-12-15 13:17:49 +0200

    Initial Commit

---
+++ SwiftIntermediate/SwiftIntermediateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftIntermediateApp.swift
+//  SwiftIntermediate
+//
+//  Created by Toni Stoyanov on 15.12.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftIntermediateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
