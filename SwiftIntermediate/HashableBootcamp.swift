95ccb737545cb1a5c6884f3eafe1ae9b036918f1
Toni Stoyanov <toni.n.stoyanov@icloud.com>
2024-12-16 09:17:29 +0200

    Hashable

---
+++ SwiftIntermediate/HashableBootcamp.swift
@@ -0,0 +1,45 @@
+//
+//  HashableBootcamp.swift
+//  SwiftIntermediate
+//
+//  Created by Toni Stoyanov on 16.12.24.
+//
+
+import SwiftUI
+
+struct MyCustomModel: Hashable {
+//    let id = UUID().uuidString
+    let title: String
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(title)
+    }
+}
+
+struct HashableBootcamp: View {
+    let data: [MyCustomModel] = [
+        MyCustomModel(title: "ONE"),
+        MyCustomModel(title: "TWO"),
+        MyCustomModel(title: "THREE"),
+        MyCustomModel(title: "FOUR"),
+        MyCustomModel(title: "FIVE"),
+    ]
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 40) {
+                ForEach(data, id: \.self) { item in
+                    Text(item.hashValue.description)
+                        .font(.headline)
+                }
+//                ForEach(data) { item in
+//                    Text(item.id)
+//                        .font(.headline)
+//                }
+            }
+        }
+    }
+}
+
+#Preview {
+    HashableBootcamp()
+}
